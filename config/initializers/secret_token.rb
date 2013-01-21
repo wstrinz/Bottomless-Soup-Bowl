ec3ec74d634684aea477bcb3da2128aadb0bba9f
Will Strinz <wstrinz@gmail.com>
2013-01-21 13:22:47 -0600

    first commit :)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BottomlessSoupBowl::Application.config.secret_token = '0fed493f88962a0959ad75d187887e7d6ea540636a40cdb58aa3b30dae7f3d8fb302de4e20d345da51ef3ef130604825237735a682c0118d765a8d53ea274ee1'
